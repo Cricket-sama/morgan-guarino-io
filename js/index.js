4da2baeac33951d98b78eb6a9fb5e9ccd0d6db95
Morgan Guarino <morgan.s.guarino@gmail.com>
2024-08-25 15:30:32 -0400

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 const footerElement = document.createElement('footer');
-// footerElement.textContent = 'My Footer';
 document.body.appendChild(footerElement);
 
 const today = new Date();
@@ -22,4 +21,51 @@ for (let i = 0; i < skills.length; i++) {
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill);
 }
+
+function checkListContents() {
+    const messageSection = document.querySelector('#messages');
+    const messageList = messageSection.querySelector('ul');
+
+    if (messageList.children.length === 0) {
+        messageSection.style.display = 'none';
+    } else {
+        messageSection.style.display = 'block';
+    }
+}
+
+checkListContents();
+
+const messageForm = document.querySelector('form[name="leave_message"]');
+
+messageForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = event.target.elements['usersName'].value;
+    const email = event.target.elements['usersEmail'].value;
+    const message = event.target.elements['usersMessage'].value;
+
+    console.log(name, email, message);
+
+    const messageSection = document.querySelector('#messages');
+    const messageList = messageSection.querySelector('ul');
+    const newMessage = document.createElement('li');
+
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a><span> : ${message} </span>`;
+
+    const removeButton = document.createElement('button');
+    removeButton.innerText = "remove";
+    removeButton.type = "button";
+    removeButton.addEventListener('click', function() {
+        const entry = removeButton.parentNode;
+        entry.remove();
+        checkListContents();
+    });
+    
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    messageForm.reset(); 
+
+    checkListContents();
+});
 
